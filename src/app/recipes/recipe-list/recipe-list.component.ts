ed8d8fb52f56f01b18e6f13babbd78815d93d3f5
Admin <admin@Admins-MacBook-Pro.local>
2017-11-22 14:59:51 +1100

    Html for all components set up

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'The full content of the test recipe', 'https://cdn-jpg2.thedailymeal.com/sites/default/files/2014/09/25/chimichurri-sauce.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
