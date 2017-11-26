b56fe70811ac8052ca95fe904ec315e2cf4a5c02
Carole <cjpillette@hotmail.com>
2017-11-26 17:17:57 +1100

    Passing ingredients to shopping List

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -16,4 +16,8 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit() {
   }
 
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
 }
