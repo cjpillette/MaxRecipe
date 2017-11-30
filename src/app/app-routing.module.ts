e495677cf0fcad4859805a6807924e73ba5460c2
Carole Pillette <carole.pillette@auspost.com.au>
2017-11-30 11:22:15 +1100

    Fixed node version and router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const appRoutes = Routes [
-  {path: '', redirectTo:'/recipes'},
+const appRoutes: Routes = [
+  {path: '', redirectTo:'/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent },
   {path: 'shopping-list', component: ShoppingListComponent }
 ];
